06955e69192172900b48836208e4e805080b02d5
Jesalim <jessyculture@gmail.com>
2023-02-11 00:54:03 +0300

    Oxford comma

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+    if array.length == 1
+      return array[0]
+    elsif array.length == 2
+      return "#{array[0]} and #{array[1]}"
+    else
+      last_element = array.pop
+      array.join(", ") + ", and #{last_element}"
+    end
+  end
+  
